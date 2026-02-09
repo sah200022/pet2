b5e46c37b89e373fd1a4c9a062ce898a8eee59b7
Timur <sah200022@gmail.com>
2026-02-09 22:18:33 +0300

    add postgres v1.0

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -4,18 +4,27 @@ import (
 	handler2 "PetProject/internal/articles/handler"
 	repository2 "PetProject/internal/articles/repository"
 	service2 "PetProject/internal/articles/service"
+	"PetProject/internal/database"
 	"PetProject/internal/handler"
 	"PetProject/internal/middleware"
 	"PetProject/internal/repository"
 	"PetProject/internal/service"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 
+	//Подключение к БД
+	dbPool, err := database.NewPostgresPool()
+	if err != nil {
+		log.Fatal(err, "Failed to connect to DB")
+	}
+	defer dbPool.Close()
+
 	mux := http.NewServeMux()
-	userRepo := repository.NewUserRepository()
+	userRepo := repository.NewUserRepository(dbPool)
 	authService := service.NewAuthService(userRepo)
 	authHandler := handler.NewAuthHandler(authService)
 
@@ -23,6 +32,7 @@ func main() {
 	articleService := service2.NewArticleService(articleRepo)
 	articleHandler := handler2.NewArticleHandler(articleService)
 
+	//Хэндлеры
 	mux.HandleFunc("/register", authHandler.Register)
 	mux.HandleFunc("/login", authHandler.Login)
 	mux.Handle("/me", middleware.JWTMiddleware(http.HandlerFunc(authHandler.Me)))
@@ -30,8 +40,9 @@ func main() {
 	mux.HandleFunc("/articles", articleHandler.GetAll)
 	mux.HandleFunc("/articles/", articleHandler.GetID)
 
+	//Запуск сервера
 	fmt.Println("Запуск сервера")
-	err := http.ListenAndServe(":8081", mux)
+	err = http.ListenAndServe(":8081", mux)
 	if err != nil {
 		fmt.Println("ошибка запуска сервера", err)
 	}
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,31 +1,53 @@
 package repository
 
-import "errors"
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type User struct {
+	ID       int    `db:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type UserRepository struct {
-	users map[string]User
+	db *pgxpool.Pool
 }
 
-func NewUserRepository() *UserRepository {
+func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{
-		users: make(map[string]User),
+		db: db,
 	}
 }
 
 func (r *UserRepository) GetMail(email string) (User, error) {
-	user, ok := r.users[email]
-	if !ok {
-		return User{}, errors.New("user not found")
+	query := `
+	SELECT id, email, password 
+	FROM users
+	WHERE email = $1;
+`
+	var user User
+
+	err := r.db.QueryRow(context.Background(), query, email).Scan(&user.ID, &user.Email, &user.Password)
+	if err != nil {
+		return User{}, err
 	}
 	return user, nil
 }
 
 func (r *UserRepository) Create(user User) (User, error) {
-	r.users[user.Email] = user
+	query := `
+		INSERT INTO users (email, password)
+		VALUES ($1, $2)
+		RETURNING id
+	`
+	var id int
+
+	err := r.db.QueryRow(context.Background(), query, user.Email, user.Password).Scan(&id)
+	if err != nil {
+		return User{}, err
+	}
+	user.ID = id
 	return user, nil
 }
